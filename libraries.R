8999cfff59cf82a51d7eb43095d0ce45586b9681
Hem Nalini Morzaria-Luna <hmorzaria@hotmail.com>
2022-12-01 15:11:26 -0800

    Modified text for jellyfish

--- libraries.R
+++ libraries.R
@@ -1,13 +1,18 @@
 install.packages("rnaturalearthhires", repos = "http://packages.ropensci.org", type = "source")
 
 .packages = c("tidyverse","devtools","readxl",
-              "sp","sf","GISTools","rgdal",
-              "rnaturalearth","rnaturalearthdata","RCurl","XML",
-              "plyr","here", "raster", "exactextractr",
-              "climateStability","robis","mgcv","lares",
-              "car","Redmonder","metan","vegan","paran",
-              "factoextra","gridExtra","corrplot","mgcViz","ggspatial",
-              "ggrepel","patchwork","png","grid", "janitor", "purrr")
+              "sp","sf","rnaturalearth",
+              "rnaturalearthdata","here", "raster", "exactextractr",
+              "glmmTMB","knitr")
+
+# .packages = c("tidyverse","devtools","readxl",
+#               "sp","sf","GISTools","rgdal",
+#               "rnaturalearth","rnaturalearthdata","RCurl","XML",
+#               "plyr","here", "raster", "exactextractr",
+#               "climateStability","robis","mgcv","lares",
+#               "car","Redmonder","metan","vegan","paran",
+#               "factoextra","gridExtra","corrplot","mgcViz","ggspatial",
+#               "ggrepel","patchwork","png","grid", "janitor", "purrr")
 
 # Install CRAN packages (if not already installed)
 .inst <- .packages %in% installed.packages()
@@ -17,7 +22,3 @@ if(length(.packages[!.inst]) > 0) install.packages(.packages[!.inst], )
 # Load packages into session 
 lapply(.packages, require, character.only=TRUE)
 
-remotes::install_github("iobis/robis")
-
-# Load packages into session 
-lapply(.packages, require, character.only=TRUE)
